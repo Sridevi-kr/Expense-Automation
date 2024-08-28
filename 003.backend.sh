10a72c68570a17d55fbfd0aad342f1ea4b2b7575
sridevi kandhalu <sridevikandhalu@sridevis-MacBook-Pro.local>
2024-08-28 12:20:51 -0500

    backend automation

---
+++ 003.backend.sh
@@ -0,0 +1,69 @@
+#!/bin/bash 
+
+COMPONENT="backend"
+LOG="/tmp/backend.log"
+APPUSER="expense"
+ROOTPASS=$1
+
+source common.sh                # This will pull all the functions and the available variables from this file and make it available locally to this script
+
+COLOR Disabling default nodejs16 version
+dnf module disable nodejs -y &>> $LOG
+stat $? 
+
+COLOR Enabling Nodejs20
+dnf module enable nodejs:20 -y &>> $LOG
+stat $? 
+
+COLOR Installing Nodejs20 
+dnf install nodejs -y &>> $LOG
+stat $? 
+
+id $APPUSER  &>> $LOG                          # This makes sure account creation will only happen if the account doesn't exist.
+if [ $? -ne 0 ]; then 
+    COLOR Creating $APPUSER Service Account 
+    useradd $APPUSER
+    stat $? 
+fi 
+
+COLOR Cleanup of old content
+rm -rf /app &>> $LOG
+stat $?
+
+COLOR Downloading $COMPONENT
+mkdir /app 
+curl -o /tmp/$COMPONENT.zip https://expense-web-app.s3.amazonaws.com/$COMPONENT.zip    &>> $LOG
+stat $? 
+
+COLOR configuring backend service 
+cp backend.service /etc/systemd/system/$COMPONENT.service 
+stat $?
+
+COLOR Extracting $COMPONENT 
+cd /app 
+unzip -o /tmp/backend.zip  &>> $LOG
+stat $? 
+
+COLOR Generating Artifacts
+npm install &>> $LOG
+stat $? 
+
+COLOR Defining Permissions To $APPUSER
+chmod -R 775 /app
+chown -R $APPUSER:$APPUSER /app
+stat $? 
+
+COLOR Installing mysql client
+dnf install mysql-server -y &>> $LOG
+stat $?
+
+COLOR Injecting Scheme To MySQL DB
+mysql -h 172.31.94.31 -uroot -p$ROOTPASS < /app/schema/backend.sql  &>> $LOG
+stat $? 
+
+COLOR Starting $COMPONENT  
+systemctl daemon-reload     &>> $LOG
+systemctl enable backend    &>> $LOG
+systemctl restart backend     &>> $LOG
+
+echo -e "\n\t ** $COMPONENT Installation Completed **"
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Expense-Automation
+
+Expense project automation will be addressed keeping the Non-Functional Requiments in to consideration.
+
+We are also going to create our onw DNS Domain both for internal and external records.
+
+
+
+For ex: shipping.flikrt.com , payment.flikrt.com   ( Public Domains : These records can be accessed from the public network )
+
+        A Records
+        100.200.120.150 ---> shipping.flikrt.com
+        100.200.130.150 ---> payment.flikrt.com
+
+        CNAME Records 
+        abc.testlb.globaldns.sampler.com    ----> global.flikrt.com 
+
+
+### How it works,  when if an org wants to move them domain name system to cloud from on-prem 
+
+    1) They create the domain with the same name on AWS Route53 
+    2) They will change the nameServer details on cloud ( Domain migration )
+
+### Internal Zones 
+
+    1) Internal Domains are unique only with in the account 
+    2) They are resolved only on the networks you mentioned.
